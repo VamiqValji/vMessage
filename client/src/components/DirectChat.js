ed46647a07570dedad0923cc93e9326320c72edb
Vamiq <vamiq.valji@gmail.com>
2021-02-06 23:34:11 -0500

    basic file structure for dm page => going to pass props down directly from DirectChatMenu to DirectChat

---
+++ client/src/components/DirectChat.js
@@ -0,0 +1,9 @@
+import "../App.css";
+
+export default function DirectChatMenu() {
+  return (
+    <>
+      <div>Chat</div>
+    </>
+  );
+}
--- client/src/pages/DirectMessages.js
+++ client/src/pages/DirectMessages.js
@@ -2,35 +2,31 @@ import React from "react";
 import "../App.css";
 import { useSelector } from "react-redux";
 import SuccessPopUp from "../components/SuccessPopUp";
-import { Link } from "react-router-dom";
+// import DirectChatMenu from "../components/DirectChatMenu";
+import DirectChat from "../components/DirectChat";
 
-export default function Home() {
+export default function DirectMessages() {
   const isLogged = useSelector((state) => state.isLogged);
 
-  const linkStyle = { textDecoration: "none", color: "white" };
-
-  const publicChat = () => {
-    return (
-      <li className="logOut">
-        <Link to="/public" style={linkStyle}>
-          Public Room
-        </Link>
-      </li>
-    );
-  };
+  let render;
+  if (isLogged) {
+    render = (
+    <div>
+        {/* <div><DirectChatMenu /></div> */}
+        <div>Direct Chat Menu</div>
+        <div><DirectChat /></div>
+        {/* going to pass down props from direct chat menu to direct chat (current room, contact, etc.) */}
+    </div>)
+  } else {
+    render = <SuccessPopUp message={"Please go login first."} color={"red"} />;
+  }
 
   return (
     <div className="App">
       <div className="contentContainer">
         <div className="loginContainer">
           <h1>Direct Messages</h1>
-          <div>
-            {isLogged ? (
-              publicChat()
-            ) : (
-              <SuccessPopUp message={"Please go login first."} color={"red"} />
-            )}
-          </div>
+          <div>{render}</div>
         </div>
       </div>
     </div>
